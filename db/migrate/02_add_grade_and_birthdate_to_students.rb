a3b0338519cc9c35567928ddca9e2488e2f95668
Dave Winniford <dwinniford@gmail.com>
2020-02-13 22:11:52 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
-  def change :
+  def change 
     add_column :students, :grade, :integer 
     add_column :students, :birthdate, :string 
   end 
